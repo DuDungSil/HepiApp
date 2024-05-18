c96ca2c5fa8f35675b432b1ef92f34aa2f48f82a
Lmincek <lmhfox3573@naver.com>
2024-05-18 18:29:54 +0900

    fix pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:flutter_app/pages/main/homePage.dart';
-import 'package:flutter_app/pages/user/loginPage.dart';
 import 'package:flutter_app/pages/main/myPage.dart';
 import 'package:flutter_app/pages/main/chattingPage.dart';
 import 'package:flutter_app/pages/main/qrPage.dart';
+import 'package:flutter_app/store/eventImages.dart';
 import 'package:flutter_app/store/products.dart';
 import 'package:flutter_app/widget/bottombar.dart';
 import 'package:flutter_app/pages/main/searchPage.dart';
@@ -13,23 +13,22 @@ import 'package:flutter_app/store/user.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
-import 'function/login.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();  // 1번코드
   await dotenv.load(fileName: "properties.env");    // 2번코드
 
-  // Flutter 애플리케이션을 시작
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (c) => user()),
         ChangeNotifierProvider(create: (c) => products()),
+        ChangeNotifierProvider(create: (c) => eventImages()),
       ],
       child: MyApp(),
     ),
   );
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
