0c98f116cf7ffb450f9dc9102c3e76bd213a67ba
DuDungSil <121501087+DuDungSil@users.noreply.github.com>
2024-07-19 04:22:20 +0900

    eventBox update

--- lib/widgets/customAppbar.dart
+++ lib/widgets/customAppbar.dart
@@ -18,49 +18,54 @@ class _CustomAppbarState extends State<CustomAppbar> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      margin: Constants.SCREEN_HORIZONTAL_MARGIN,
-      padding: EdgeInsets.fromLTRB(0, 5, 0, 10),
-      decoration: BoxDecoration(color: Color(0xFFFFFFFF)),
-      height: (widget.child == null) ? Constants.APPBAR_TITLE_HEIGHT : Constants.APPBAR_TITLE_HEIGHT + Constants.APPBAR_CONTENT_HEIGHT,
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: widget.leading ?? SizedBox(width: 28), // 왼쪽 버튼 (leading)
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  widget.title,
-                  style: GoogleFonts.getFont(
-                    'Roboto Condensed',
-                    fontWeight: FontWeight.w600,
-                    fontSize: 20,
-                    height: 1.2,
-                    letterSpacing: -0.5,
-                    color: Color(0xFF111111),
+      decoration: BoxDecoration(
+        color: Color(0xFFFFFFFF),
+      ),
+      child: Container(
+        alignment: Alignment.center,
+        margin: Constants.SCREEN_HORIZONTAL_MARGIN,
+        padding: EdgeInsets.fromLTRB(0, 5, 0, 10),
+        decoration: BoxDecoration(color: Color(0xFFFFFFFF)),
+        height: (widget.child == null) ? Constants.APPBAR_TITLE_HEIGHT : Constants.APPBAR_TITLE_HEIGHT + Constants.APPBAR_CONTENT_HEIGHT,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: widget.leading ?? SizedBox(width: 28), // 왼쪽 버튼 (leading)
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    widget.title,
+                    style: GoogleFonts.getFont(
+                      'Roboto Condensed',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20,
+                      height: 1.2,
+                      letterSpacing: -0.5,
+                      color: Color(0xFF111111),
+                    ),
                   ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: widget.trailing != null
-                    ? Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: widget.trailing!,
-                )
-                    : SizedBox(width: 28), // 오른쪽 버튼들 (trailing)
-              ),
-            ],
-          ),
-          if (widget.child != null) widget.child!,
-        ],
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: widget.trailing != null
+                      ? Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: widget.trailing!,
+                        )
+                      : SizedBox(width: 28), // 오른쪽 버튼들 (trailing)
+                ),
+              ],
+            ),
+            if (widget.child != null) widget.child!,
+          ],
+        ),
       ),
     );
   }
